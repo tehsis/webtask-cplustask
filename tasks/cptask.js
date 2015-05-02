fb07ae0abb029e573e6521923a9415e279cf0a5c
Pablo Terradillos <tehsis@gmail.com>
2015-05-02 09:12:32 -0300

    fixed task

--- tasks/cptask.js
+++ tasks/cptask.js
@@ -5,12 +5,14 @@ var input = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.cpp';
 var output = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.out';
 
 return function(context, cb) {
-  fs.write(input, context.data.code, 'utf-8', function(err) {
-    if (err) return cb(err);
-    cp.exec('g++ ' + input + ' -o ' + output , function(err) {
+  fs.open(input, 'w', function(err, fd) {
+    fs.write(fd, context.data.code, 'utf-8', function(err) {
       if (err) return cb(err);
-      fs.readFile(output, function(err, data) {
-        cb(err, data);
+      cp.exec('g++ ' + input + ' -o ' + output , function(err) {
+        if (err) return cb(err);
+        fs.readFile(output, function(err, data) {
+          cb(err, data);
+        });
       });
     });
   });
