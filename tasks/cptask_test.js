d0563b69f358fef0467129548c046f2251683b86
Pablo Terradillos <tehsis@gmail.com>
2015-05-02 16:22:49 -0300

    Update cptask_test.js

--- tasks/cptask_test.js
+++ tasks/cptask_test.js
@@ -1,3 +1,20 @@
+var fs = require('fs');
+var cp = require('child_process');
+var crypto = require('crypto');
+
+var input = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.cpp';
+var output = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.out';
+
 return function(context, cb) {
-  cb(null, context);
+  fs.open(input, 'w', function(err, fd) {
+    fs.write(fd, context.data.script, 'utf-8', function(err) {
+      if (err) return cb(err);
+      cp.exec('g++ ' + input + ' -o ' + output , function(err) {
+        if (err) return cb(err);
+        cp.exec(output, function(err, out) {
+          cb(err, out);
+        });
+      });
+    });
+  });
 }
