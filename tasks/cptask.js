fdf6be660816e8fbfb06b17afc4c45089e0503b7
Pablo Terradillos <tehsis@gmail.com>
2015-05-02 09:18:27 -0300

    fixed crypto in task

--- tasks/cptask.js
+++ tasks/cptask.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 var cp = require('child_process');
+var crypto = require('crypto');
 
 var input = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.cpp';
 var output = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.out';
 
-return function(context, cb) {
   fs.open(input, 'w', function(err, fd) {
-    fs.write(fd, context.data.code, 'utf-8', function(err) {
+    fs.write(fd, 'test', 'utf-8', function(err) {
       if (err) return cb(err);
       cp.exec('g++ ' + input + ' -o ' + output , function(err) {
         if (err) return cb(err);
@@ -16,4 +16,3 @@ return function(context, cb) {
       });
     });
   });
-};
