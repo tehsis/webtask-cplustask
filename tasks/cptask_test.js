099255ae6ded508d129d083d1fbb33e04d70b0d8
Pablo Terradillos <tehsis@gmail.com>
2015-05-02 16:23:41 -0300

    Update cptask_test.js

--- tasks/cptask_test.js
+++ tasks/cptask_test.js
@@ -7,7 +7,7 @@ var output = '/tmp/' + crypto.randomBytes(5).toString('hex') + '.out';
 
 return function(context, cb) {
   fs.open(input, 'w', function(err, fd) {
-    fs.write(fd, context.data.script, 'utf-8', function(err) {
+    fs.write(fd, context.body, 'utf-8', function(err) {
       if (err) return cb(err);
       cp.exec('g++ ' + input + ' -o ' + output , function(err) {
         if (err) return cb(err);
